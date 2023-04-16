6acf5197af4e482480fa1149d800601eb93accab
Vikas preet Tiwana <vikastiwana8@gmail.com>
2023-04-16 15:58:01 -0700

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching (drivers, name){
+    return drivers.filter (function (driver) { 
+        return driver.toLowerCase() === name.toLowerCase();
+     })
+}
+
+function fuzzyMatch (drivers, querry){
+    return drivers.filter (function (driver){
+        return (driver.toLowerCase().substring(0, querry.length) === querry.toLowerCase())
+    }
+    )    
+}
+
+const driversobj = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+  
+function matchName (driversobj, value){
+    return driversobj.filter((function (arr){
+        return arr.name === value;
+    }
+     ) )
+}
